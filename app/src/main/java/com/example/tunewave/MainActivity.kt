852d2abe5e24c6fc7bceaa4820bf943685006f1e
AnaSantana016 <anasantanaojeda.16@gmail.com>
2023-11-18 17:56:05 +0100

    Añadido el botón de back y de registro, además de los campos de nombre y email

--- app/src/main/java/com/example/tunewave/MainActivity.kt
+++ app/src/main/java/com/example/tunewave/MainActivity.kt
@@ -1,34 +1,20 @@
 package com.example.tunewave
 
-import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.ArrowBack
-import androidx.compose.material.icons.outlined.Email
-import androidx.compose.material.icons.outlined.Lock
-import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
@@ -40,16 +26,9 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalSoftwareKeyboardController
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.platform.SoftwareKeyboardController
 import androidx.compose.ui.text.input.ImeAction
-import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
-import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.tunewave.ui.theme.TuneWaveTheme
@@ -64,149 +43,98 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    //RegisterForm()
+                    RegisterForm()
                 }
             }
         }
     }
 }
-/*
-@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)
+
+@OptIn(ExperimentalMaterial3Api::class, ExperimentalComposeUiApi::class)
 @Composable
 fun RegisterForm() {
     var email by remember { mutableStateOf("") }
     var name by remember { mutableStateOf("") }
     var password by remember { mutableStateOf("") }
-    var isPasswordVisible by remember { mutableStateOf(false) }
-    var imageUri by remember { mutableStateOf<Uri?>(null) }
-
-    var keyboardController = LocalSoftwareKeyboardController.current
-
-    val context = LocalContext.current
 
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
-        horizontalAlignment = Alignment.CenterHorizontally
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
     ) {
+        Text(
+            text = "Back",
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(8.dp)
+                .clickable { /* Handle back button click */ }
+        )
+
         Spacer(modifier = Modifier.height(16.dp))
 
-        OutlinedTextField(
+        TextField(
             value = email,
             onValueChange = { email = it },
             label = { Text("Email") },
-            leadingIcon = { Icon(Icons.Outlined.Email, contentDescription = null) },
-            keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Email,
-                imeAction = ImeAction.Next
-            ),
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 8.dp)
+                .padding(8.dp)
         )
 
-        OutlinedTextField(
+        Spacer(modifier = Modifier.height(8.dp))
+
+        TextField(
             value = name,
             onValueChange = { name = it },
             label = { Text("Name") },
-            leadingIcon = { Icon(Icons.Outlined.Person, contentDescription = null) },
-            keyboardOptions = KeyboardOptions.Default.copy(
-                imeAction = ImeAction.Next
-            ),
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 8.dp)
+                .padding(8.dp)
         )
 
-        var visualTransformation = if (isPasswordVisible) VisualTransformation.None else PasswordVisualTransformation()
+        Spacer(modifier = Modifier.height(8.dp))
 
-        OutlinedTextField(
+        // Utilizar un lambda para encapsular el Text("Password")
+        /*val passwordLabel: @Composable (Modifier) -> Unit = {
+            Text("Password", modifier = it)
+        }
+
+        TextField(
             value = password,
             onValueChange = { password = it },
-            label = { Text("Password") },
-            leadingIcon = { Icon(Icons.Outlined.Lock, contentDescription = null) },
+            label = passwordLabel,
+            visualTransformation = PasswordVisualTransformation(),
             keyboardOptions = KeyboardOptions.Default.copy(
-                keyboardType = KeyboardType.Password,
                 imeAction = ImeAction.Done
             ),
-            visualTransformation = visualTransformation,
-            trailingIcon = {
-                IconButton(
-                    onClick = {
-                        isPasswordVisible = !isPasswordVisible
-                    }
-                ) {
-                    Icon(
-                        if (isPasswordVisible) Icons.Outlined.Visibility else Icons.Outlined.VisibilityOff,
-                        contentDescription = if (isPasswordVisible) "Hide Password" else "Show Password"
-                    )
+            onImeActionPerformed = { action: ImeAction, controller: SoftwareKeyboardController? ->
+                if (action == ImeAction.Done) {
+                    controller?.hide()
                 }
             },
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(bottom = 8.dp)
-        )
+                .padding(8.dp)
+        )*/
 
-        // Image Picker
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(200.dp)
-                .clip(shape = MaterialTheme.shapes.medium)
-                .background(MaterialTheme.colorScheme.primary)
-                .clickable {
-                    val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
-                    context.startActivityForResult(intent, 1)
-                },
-            contentAlignment = Alignment.Center
-        ) {
-            if (imageUri != null) {
-                Image(
-                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .clip(shape = MaterialTheme.shapes.medium),
-                    contentScale = ContentScale.Crop
-                )
-            } else {
-                Column(
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Icon(
-                        imageUri = Icons.Outlined.CloudUpload,
-                        contentDescription = null,
-                        modifier = Modifier.size(24.dp)
-                    )
-                    Spacer(modifier = Modifier.height(8.dp))
-                    Text(text = "Upload Image", color = Color.White)
-                }
-            }
-        }
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        // Register Button
         Button(
-            onClick = {
-                // TODO: Handle registration logic
-            },
+            onClick = { /* Handle registration */ },
             modifier = Modifier.fillMaxWidth()
         ) {
             Text("Register")
         }
-
-        // Back Button
-        IconButton(
-            onClick = {
-                // TODO: Handle back button click
-            },
-            modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(16.dp)
-        ) {
-            Icon(Icons.Outlined.ArrowBack, contentDescription = "Back")
-        }
     }
-}*/
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewRegisterForm() {
+    TuneWaveTheme {
+        RegisterForm()
+    }
+}
