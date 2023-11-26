cb3e528ff31cc510ca12f641347f87e8814a7be8
Alejandro  David Arzola Saavedra <90756437+AlejandroDavidArzolaSaavedra@users.noreply.github.com>
2023-11-26 16:27:05 +0000

    Update loginUseCase.kt

--- app/src/main/java/com/example/tunewave/domain/loginUseCase.kt
+++ app/src/main/java/com/example/tunewave/domain/loginUseCase.kt
@@ -1,6 +1,15 @@
 package com.example.tunewave.domain
 
-sealed class LoginResult {
-    object Error : LoginResult()
-    data class Success(val verified: Boolean) : LoginResult()
+import com.example.tunewave.data.database.AuthenticationService
+import com.example.tunewave.data.database.UserService
+import com.example.tunewave.ui.singIn.user.UserModel
+import javax.inject.Inject
+
+class LogInUseCase @Inject constructor(
+    private val authenticationService: AuthenticationService,
+) {
+     operator fun invoke(userSignIn: UserModel) {
+        authenticationService.login(userSignIn.email, userSignIn.password) != null
+        //userService.uploadImage("/users", userSignIn.urlImage)
+    }
 }
